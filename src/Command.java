d41057409fa5b0373807f0c7e5b093874da92da5
jho951 <jho951@naver.com>
2024-11-19 14:17:10 +0900

    [FEAT] 명령 생성

---
+++ src/Command.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Command {
+    public void command() {
+        Scanner scanner = new Scanner(System.in);
+
+        String input  = scanner.nextLine();
+        System.out.println(input);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("명령 )");
+        Command cmd = new Command(); // Command 클래스 사용
+        cmd.command();
+        System.out.print("종료");
+    }
+}

84fda7022053da6b83aad30a5737e1e56d25e23d
jho951 <jho951@naver.com>
2024-11-19 14:45:33 +0900

    [FEAT] create project

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Command cmd = new Command(); // Command 클래스 사용
-
-        String input; // 입력 변수 선언
+        String wiseSaying; // 입력 변수 선언
 
         do {
             System.out.print("명령 ) ");
-            input = scanner.nextLine(); // 사용자 입력 받기
-            if (!input.equals("exit")) { // "종료"가 아닌 경우만 명령 실행
-                cmd.command(input);     // Command 클래스의 command 메서드 호출
+            wiseSaying = scanner.nextLine(); // 사용자 입력 받기
+            if (!wiseSaying.equals("exit")) { // "종료"가 아닌 경우만 명령 실행
+                cmd.command(wiseSaying);     // Command 클래스의 command 메서드 호출
             }
-        } while (!input.equals("exit")); // "종료"일 때 루프 종료
+        } while (!wiseSaying.equals("exit")); // "종료"일 때 루프 종료
 
         System.out.println("종료"); // 종료 메시지 출력
         scanner.close();

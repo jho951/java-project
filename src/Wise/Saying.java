96ff857de2ba12aad7c0ecca8cdb720dcd09b65d
jho951 <jho951@naver.com>
2024-11-21 16:14:48 +0900

    [FEAT] create wise-saying level1

--- src/Wise/Saying.java
+++ src/Wise/Saying.java
@@ -7,7 +7,15 @@ public class Saying {
         while (true) {
             System.out.print("명령 ) ");
             String inputCommand = scanner.nextLine();
-
+            if(inputCommand.equalsIgnoreCase("등록")) {
+                System.out.println("명언을 입력해주세요.");
+                String wiseSaying = scanner.nextLine();
+                System.out.printf("명언 : %s \n", wiseSaying);
+                System.out.println("작가를 입력해주세요.");
+                String author = scanner.nextLine();
+                System.out.printf("작가 : %s", author);
+                break;
+            }
             if (inputCommand.equalsIgnoreCase("종료")) {
                 break;
             }

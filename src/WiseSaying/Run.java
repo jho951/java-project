fbcc5e22c9dc2789a8cdb99c8f0f9d7a4e3578a6
jho951 <jho951@naver.com>
2024-11-29 15:18:22 +0900

    [FEAT] refetorying level1

--- src/WiseSaying/Run.java
+++ src/WiseSaying/Run.java
@@ -10,22 +10,40 @@ public class Run {
     public static void run() {
         try (Scanner scanner = new Scanner(System.in)) {
             while (true) {
-                System.out.print("명령 ) ");
-                String inputCommand = scanner.nextLine().trim(); // 경우 수 지우기
-                if(inputCommand.equals(REGISTER_COMMAND)) {
+                System.out.print("명령어 : ");
+                String inputCommand = scanner.nextLine().trim(); // 명령어 입력
+                if (inputCommand.equals(REGISTER_COMMAND)) {
                     System.out.println("명언을 입력해주세요.");
-                    System.out.print("명언: ");
-                    String wiseSayingCommand = scanner.nextLine().trim();
-                    WiseSaying.WiseSayingList item = new WiseSaying.WiseSayingList(wiseSayingCommand, "ㄴㅇㄹ");
-                    System.out.println("등록된 명언 ID: " + item.num + ", 명언: " + item.title + ", 작가: " + item.author);
+                    String wiseSayingCommand;
+                    String authorCommand;
+                    while (true) {
+                        System.out.print("명언: ");
+                        wiseSayingCommand = scanner.nextLine().trim();
+                        if (wiseSayingCommand.isEmpty()) {
+                            System.out.println("명언은 빈칸일 수 없습니다. 다시 입력해주세요.");
+                        } else {
+                            break; // 명언이 비어있지 않으면 반복 종료
+                        }
+                    }
+                    System.out.println("작가를 입력해주세요.");
+                    while (true) {
+                        System.out.print("작가: ");
+                        authorCommand = scanner.nextLine().trim();
+                        if (authorCommand.isEmpty()) {
+                            System.out.println("작가는 빈칸일 수 없습니다. 다시 입력해주세요.");
+                        } else {
+                            break; // 명언이 비어있지 않으면 반복 종료
+                        }
+                    }
+                    WiseSaying.WiseSayingList item = new WiseSaying.WiseSayingList(wiseSayingCommand, authorCommand);
+                    System.out.printf("%d번째 명언이 등록되었습니다.\n", item.num);
+                    System.out.printf("명언: %s, 작가: %s \n", item.title, item.author);
                     continue;
                 }
-                // 공백만 입력한 경우 무시
                 if (inputCommand.isEmpty()) {
-                    System.out.println("명령을 입력해주세요.");
+                    System.out.println("명령어를 입력해주세요.");
                     continue;
                 }
-                // 종료 명령어 확인
                 if (inputCommand.equalsIgnoreCase(EXIT_COMMAND)) {
                     System.out.println("프로그램을 종료합니다.");
                     break;
@@ -34,3 +52,4 @@ public class Run {
         }
     }
 }
+

944921f484ea29e7f0b55021ace22bf62a3d1998
jho951 <jho951@naver.com>
2024-12-27 20:02:09 +0900

    [CREATE] reTest4 branch

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        int wiseNumber = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
         while (true) {
@@ -17,6 +18,8 @@ public class App {
                scanner.nextLine();
                System.out.print("작가: ");
                scanner.nextLine();
+               wiseNumber++;
+               System.out.printf("%d번 명언이 등록되었습니다.\n",wiseNumber);
            }
        }
         scanner.close();

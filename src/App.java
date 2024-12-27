249b290279c0dd3ed22f5164ba966082399b818a
jho951 <jho951@naver.com>
2024-12-27 19:58:48 +0900

    [CREATE] reTest3

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        int wiseNum = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
         while (true) {
@@ -17,8 +17,6 @@ public class App {
                scanner.nextLine();
                System.out.print("작가: ");
                scanner.nextLine();
-               wiseNum++;
-               System.out.printf("%d번 명령이 등록되었습니다.\n",wiseNum);
            }
        }
         scanner.close();

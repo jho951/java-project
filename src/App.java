6b671112bf942b5fce065b74cd08510ce86b5dd5
jho951 <jho951@naver.com>
2024-12-27 14:04:45 +0900

    [CREATE] reTest2

--- src/App.java
+++ src/App.java
@@ -4,7 +4,15 @@ public class App {
     public static void main(String[] args) {
        Scanner scanner = new Scanner(System.in);
        System.out.println("== 명언 앱 ==");
+       System.out.print("명령: ");
        String command = scanner.nextLine();
-       System.out.printf("명령: %s", command);
+       if(command.equals("register")){
+           System.out.print("명언: ");
+           scanner.nextLine();
+           System.out.print("작가: ");
+           scanner.nextLine();
+       }
+       System.out.print("== 종료 ==");
+       scanner.close();
     }
 }

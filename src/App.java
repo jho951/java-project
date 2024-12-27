1e5e934b6e97cbc18d6da1db2455bfa2a31230fa
jho951 <jho951@naver.com>
2024-12-27 14:42:15 +0900

    [CREATE] reTest3 branch

--- src/App.java
+++ src/App.java
@@ -2,18 +2,25 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-       System.out.println("== 명언 앱 ==");
-       System.out.print("명령: ");
-       String command = scanner.nextLine();
-       if(command.equals("register")){
-           System.out.print("명언: ");
-           scanner.nextLine();
-           System.out.print("작가: ");
-           scanner.nextLine();
-           System.out.println("1번 명령이 등록되었습니다.");
+        int wiseNum = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("== 명언 앱 ==");
+        while (true) {
+           System.out.print("명령: ");
+           String command = scanner.nextLine();
+           if(command.equals("exit")){
+               System.out.println("==종료==");
+               break;
+           }
+           if(command.equals("register")){
+               System.out.print("명언: ");
+               scanner.nextLine();
+               System.out.print("작가: ");
+               scanner.nextLine();
+               wiseNum++;
+               System.out.printf("%d번 명령이 등록되었습니다.\n",wiseNum);
+           }
        }
-       System.out.print("== 종료 ==");
-       scanner.close();
+        scanner.close();
     }
 }
